61b4469f945b6d9f32541f5f0d00145cc11f39a7
m266534 <148171584+m266534@users.noreply.github.com>
2023-10-17 15:10:46 -0400

    A "game" was created that displays a blue rectangle and a brown rectangle

--- game.py
+++ game.py
@@ -1,3 +1,33 @@
-print("Hello World")
-x = "How are you doing?"
-print(x)
+import pygame
+import sys
+
+pygame.init()
+
+screen_width = 500
+screen_height = 500
+
+blue = (0,0,255)
+brown = (244,164,96)
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Blue is the Background with a Brown Rectangle")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    screen.fill(blue)
+
+
+    rectangle_height = 250
+    pygame.draw.rect(screen, brown, (0, screen_height - rectangle_height, screen_width, rectangle_height))
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
+
+
