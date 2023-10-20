8889a411c5f4b5e6478a4bf82ee426a7652bde9e
m266534 <148171584+m266534@users.noreply.github.com>
2023-10-19 23:13:04 -0400

    Added 2 games

--- extra_game1.py
+++ extra_game1.py
@@ -12,6 +12,15 @@ brown = (244,164,96)
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Blue is the Background with a Brown Rectangle")
 
+#create a surface
+surf = pygame.Surface((50,50))
+
+#give surface a color, seperate from the background
+surf.fill((0,0,0))
+rect = surf.get_rect()
+
+surf_center = (screen_width/2 - surf.get_width()/2, screen_height/2 - surf.get_height()/2)
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -23,9 +32,8 @@ while running:
     rectangle_height = 250
     pygame.draw.rect(screen, brown, (0, screen_height - rectangle_height, screen_width, rectangle_height))
 
+    screen.blit(surf, surf_center)
     pygame.display.flip()
 
 pygame.quit()
 sys.exit()
-
-
